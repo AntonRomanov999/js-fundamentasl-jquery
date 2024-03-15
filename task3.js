bd67d0f934f3be0f8b9da950c8e7863f36c6ee39
unknown <romanovanmih@gmail.com>
2024-03-15 16:11:38 +0200

    third task

--- task3.js
+++ task3.js
@@ -2,3 +2,9 @@
 // і перемістіть кожен <div>-елемент так, щоб він став безпосередньо над <h3>.
 
 // Завдання виділене вставкою Task3
+
+const divs = $(".elements-direction").find("div");
+const headers = $(".elements-direction").find("h3");
+
+divs.each((i, el) => {$(el).prependTo(headers[i])})
+
